2d5f1f3856b286710d5703202555d9901e68c22a
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-05-15 00:03:09 +0530

    Assignment

--- RowsColumns.java
+++ RowsColumns.java
@@ -2,11 +2,13 @@ public class RowsColumns{
 	
 	public static void main(String[] args) {
 		
-		for(int i=4;i>=1;i--){
-			for(int k=1;k<i;k++){
-				System.out.print(k + "  ");
+		for(int i=3;i>=1;i--){
+			for(int k=1;k<=i;k++){
+				System.out.print(i+ "  ");
 			}
 			System.out.println();
 		}
 	}
 }
+
+
