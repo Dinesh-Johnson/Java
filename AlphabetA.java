0ebbd9731ffec6cce55a8493d0251d560b543483
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-05-15 10:21:25 +0530

    Assignment

---
+++ AlphabetA.java
@@ -0,0 +1,18 @@
+public class AlphabetA{
+	
+	public static void main(String[] args) {
+		char $ = 65;
+		
+		for(int i=1; i<=3; i++){
+			if(i%2 !=0){
+			System.out.print($);
+			}
+
+			for(int j=1; j<=1; j++){
+				System.out.print($);
+				}
+				System.out.println();
+			}
+		
+	}
+}
---
+++ And.java
@@ -0,0 +1,17 @@
+public class And{
+	
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=3; i++){
+			if(i%2 ==0){
+			System.out.print("&");
+			}
+
+			for(int j=1; j<2; j++){
+				System.out.print("&");
+				}
+				System.out.println();
+			}
+		
+	}
+}
