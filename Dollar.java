267b5dd4315952797b0d080df63a30f6c426d980
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-05-15 10:11:56 +0530

    Assignment

--- Dollar.java
+++ Dollar.java
@@ -3,7 +3,9 @@ public class Dollar{
 	public static void main(String[] args) {
 		
 		for(int i=1; i<=3; i++){
+			if(i%2 !=0){
 			System.out.print("$");
+			}
 
 			for(int j=1; j<=2; j++){
 				System.out.print("$");
